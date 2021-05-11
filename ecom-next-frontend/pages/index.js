eed20863cc80bd9c2d3e49b398a08ac0527d3fc8
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-11 07:59:47 +0100

    Title

--- ecom-next-frontend/pages/index.js
+++ ecom-next-frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ products }) {
   return (
     <div>
       <Head>
-        <title>Build an Ecommerce with NextJS, Magic, Strapi and Stripe</title>
+        <title>Ecommerce with NextJS, Magic, Strapi and Stripe</title>
         <meta name="description" content="Learn how to build a FullStack Ecommerce in this 2 hours and a half free video sponsored by Magic" />
       </Head>
 
